b8f4914963d1754d04748f38dbda851cd710bee6
liiadavid <liiadavid@yandex.ru>
2025-09-24 20:26:39 +0300

    RCA-05 подключение и настройка кастомных шрифтов

---
+++ app/src/main/java/com/liiadavid/recipecomposeapp/ui/theme/FontFamily.kt
@@ -0,0 +1,15 @@
+package com.liiadavid.recipecomposeapp.ui.theme
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import com.liiadavid.recipecomposeapp.R
+
+val montserratFontFamily = FontFamily(
+    Font(R.font.montserrat_regular, FontWeight.Normal),
+    Font(R.font.montserrat_medium, FontWeight.Medium),
+)
+
+val montserratAlternatesFontFamily = FontFamily(
+    Font(R.font.montserratalternates_semibold, FontWeight.SemiBold),
+)
